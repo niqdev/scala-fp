9d59757985bacac4aa54d71f31d985b13a829e16
niqdev <niqdev@gmail.com>
2019-11-13 14:23:56 +0000

    eco: draft cats specs

---
+++ modules/ecosystem/src/test/scala/com/github/niqdev/cats/EqSpec.scala
@@ -0,0 +1,62 @@
+package com.github.niqdev.cats
+
+import cats.Eq
+import org.scalatest.{Matchers, WordSpecLike}
+
+final case class Cat(name: String, age: Int, color: String)
+
+object CatInstances {
+  import cats.instances.int.catsKernelStdOrderForInt
+  import cats.instances.string.catsKernelStdOrderForString
+  import cats.syntax.eq.catsSyntaxEq
+
+  implicit val catEq: Eq[Cat] = {
+    Eq.instance[Cat] { (cat1, cat2) =>
+      cat1.name === cat2.name &&
+        cat1.age === cat2.age &&
+        cat1.color === cat2.color
+    }
+  }
+}
+
+// FIXME define outside: conflicts with org.scalactic.TripleEqualsSupport
+object EqHelper {
+  import cats.syntax.eq.catsSyntaxEq
+
+  def verifyEqvSyntax[T](x: T, y: T)(implicit ev: Eq[T]): Boolean =
+    x === y
+
+  def verifyNeqvSyntax[T](x: T, y: T)(implicit ev: Eq[T]): Boolean =
+    x =!= y
+}
+
+final class EqSpec extends WordSpecLike with Matchers {
+
+  "Eq" should {
+
+    /* FIXME weird compilation error: type mismatch
+    "verify" {
+      import cats.instances.int.catsKernelStdOrderForInt
+
+      Eq[Int].eqv(123, 123) shouldBe true
+      Eq[Int].neqv(123, 456) shouldBe false
+    }
+    */
+
+    "verify instances" in {
+      import CatInstances.catEq
+      import cats.instances.option.catsKernelStdEqForOption
+
+      val cat1 = Cat("Garfield", 35, "orange and black")
+      val cat2 = Cat("Heathcliff", 30, "orange and black")
+
+      EqHelper.verifyEqvSyntax(cat1, cat2) shouldBe false
+      EqHelper.verifyNeqvSyntax(cat1, cat2) shouldBe true
+      Eq[Cat].eqv(cat1, cat2) shouldBe false
+      Eq[Cat].neqv(cat1, cat2) shouldBe true
+
+      EqHelper.verifyEqvSyntax(Option(cat1), Option.empty[Cat]) shouldBe false
+      EqHelper.verifyNeqvSyntax(Option(cat1), Option(cat2)) shouldBe true
+    }
+  }
+}
---
+++ modules/ecosystem/src/test/scala/com/github/niqdev/cats/ShowSpec.scala
@@ -0,0 +1,54 @@
+package com.github.niqdev.cats
+
+import java.util.Date
+
+import cats.Show
+import cats.syntax.show.toShow
+import org.scalatest.{Matchers, WordSpecLike}
+
+final class ShowSpec extends WordSpecLike with Matchers {
+
+  "Show" should {
+
+    /* FIXME weird compilation error: type mismatch
+    "verify apply" {
+      import cats.instances.int.catsStdShowForInt
+      import cats.instances.string.catsStdShowForString
+
+      Show.apply[Int].show(123) shouldBe "123"
+      Show[String].show("abc") shouldBe "abc"
+    }
+    */
+
+    "verify Cat" in {
+      import cats.instances.int.catsStdShowForInt
+      import cats.instances.string.catsStdShowForString
+
+      final case class Cat(name: String, age: Int, color: String)
+
+      implicit val catShow: Show[Cat] = Show.show { cat =>
+        val name = cat.name.show
+        val age = cat.age.show
+        val color = cat.color.show
+        s"$name is a $age year-old $color cat."
+      }
+
+      val cat = Cat("Garfield", 8, "ginger and black")
+      val expected = "Garfield is a 8 year-old ginger and black cat."
+
+      Show[Cat](catShow).show(cat) shouldBe expected
+      cat.show shouldBe expected
+    }
+
+    "verify Date" in {
+      implicit val dateShow: Show[Date] =
+        Show.show(date => s"${date.getTime} ms since the epoch")
+
+      val date = new Date(123)
+      val expected = "123 ms since the epoch"
+
+      Show.apply[Date](dateShow).show(date) shouldBe expected
+      date.show shouldBe expected
+    }
+  }
+}
