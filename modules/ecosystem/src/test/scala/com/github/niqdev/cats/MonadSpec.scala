7efb6a47e8b0bd927810f6f454b2f4c259ec05b6
niqdev <niqdev@gmail.com>
2019-11-14 10:47:27 +0000

    fp: update cats.MonadSpec

--- modules/ecosystem/src/test/scala/com/github/niqdev/cats/MonadSpec.scala
+++ modules/ecosystem/src/test/scala/com/github/niqdev/cats/MonadSpec.scala
@@ -138,6 +138,37 @@ final class MonadSpec extends WordSpecLike with Matchers {
       writerFor.value shouldBe 9
       writerFor.run shouldBe Tuple2(Vector("add-2", "add-3", "add-4", "end"), 9)
     }
+
+    "verify Reader" in {
+      import cats.data.Reader
+      import cats.syntax.applicative.catsSyntaxApplicativeId
+
+      final case class Db(users: Map[Int, String], passwords: Map[String, String])
+
+      type DbReader[T] = Reader[Db, T]
+
+      def findUser(userId: Int): DbReader[Option[String]] =
+        Reader(db => db.users.get(userId))
+
+      def checkPassword(user: String, password: String): DbReader[Boolean] =
+        Reader(db => db.passwords.get(user).contains(password))
+
+      def checkLogin(userId: Int, password: String): DbReader[Boolean] =
+        for {
+          maybeUser <- findUser(userId)
+          isValid <- maybeUser
+            .map(user => checkPassword(user, password))
+            .getOrElse(false.pure[DbReader])
+        } yield isValid
+
+      val users = Map(1 -> "user1", 2 -> "user2", 3 -> "user3")
+      val passwords = Map("user1" -> "password1", "user2" -> "password2", "user3" -> "password3")
+      val db = Db(users, passwords)
+
+      checkLogin(1, "password1").run(db) shouldBe true
+      checkLogin(8, "password1").run(db) shouldBe false
+      checkLogin(1, "invalid").run(db) shouldBe false
+    }
   }
 
 }
