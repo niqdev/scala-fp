18565b82f0e3cba3f2909dad779f82db9180e4da
niqdev <niqdev@users.noreply.github.com>
2020-04-01 17:35:57 +0100

    draft ScalaCheckJson

---
+++ modules/ecosystem/src/test/scala/com/github/niqdev/circe/CirceJson.scala
@@ -0,0 +1,52 @@
+package com.github.niqdev.circe
+
+import eu.timepit.refined.string.Uuid
+import eu.timepit.refined.types.string.NonEmptyString
+import io.circe.{ Json, JsonObject }
+import org.scalacheck.Gen
+
+// TODO see ScalaCheckJson
+trait CirceJson[T] {
+  def gen(value: T): Gen[Json]
+}
+
+object CirceJson {
+
+  def apply[T](implicit ev: CirceJson[T]): CirceJson[T] = ev
+
+  implicit val stringCirceJson: CirceJson[String] =
+    value => Gen.const(Json.fromString(value))
+
+  implicit val longCirceJson: CirceJson[Long] =
+    value => Gen.const(Json.fromLong(value))
+
+  implicit val uuidCirceJson: CirceJson[Uuid] =
+    value => Gen.const(Json.fromString(value.toString))
+
+  implicit def genCirceJson[T](
+    implicit circeJson: CirceJson[T]
+  ): CirceJson[Gen[T]] =
+    _.flatMap(circeJson.gen)
+
+  // TODO
+  def apply(values: (NonEmptyString, Gen[Json])*): Gen[JsonObject] =
+    values.foldLeft(Gen.const(JsonObject.empty)) { (result, data) =>
+      val (key, gen) = data
+      result.flatMap { jsonObject => gen.map(value => jsonObject.add(key.value, value)) }
+    }
+}
+
+object CirceJsonApp extends App {
+
+  // TODO newtype Key
+  implicit class KeyOps(value: String) {
+    val key: NonEmptyString =
+      NonEmptyString.unsafeFrom(value)
+  }
+
+  println(
+    CirceJson(
+      "aaa".key -> CirceJson[Gen[String]].gen(Gen.alphaNumStr)
+    ).sample
+  )
+}
---
+++ modules/ecosystem/src/test/scala/com/github/niqdev/scalacheck/ScalaCheckJson.scala
@@ -0,0 +1,92 @@
+package com.github.niqdev.scalacheck
+
+import java.io.Serializable
+
+import com.github.niqdev.scalacheck.GenJson._
+import io.circe.Json
+import org.scalacheck.Gen
+import org.scalactic.anyvals.NonEmptyString
+
+// TODO
+trait ScalaCheckJson[T] {
+  def gen(item: T): Gen[Json]
+}
+
+sealed abstract class GenJson extends Product with Serializable
+
+// TODO KEY = NonEmptyString newtype
+object GenJson {
+  final case class GenBoolean(key: String)             extends GenJson
+  final case class GenNumber(key: String)              extends GenJson // parametrize: Int, Double ...
+  final case class GenString(key: String)              extends GenJson
+  final case class Gen[T](key: String)                 extends GenJson
+  final case class GenArray[T <: GenJson](key: String) extends GenJson
+  final case class GenObject(value: GenJson*)          extends GenJson
+}
+
+object GenJson0 {
+  def apply(gen: Gen[Json]*): Gen[Json] = ???
+
+  def apply[T](key: String): Gen[Json] = ???
+
+  def apply[T](key: String, value: T): Gen[Json] = ???
+}
+
+object Hello {
+
+  val expected =
+    """
+      |{
+      |  "myBoolean": true,
+      |  "myInt": 123,
+      |  "myString": "aaa",
+      |  "myArray": [
+      |    "a",
+      |    "b",
+      |    "c"
+      |  ],
+      |  "myObject": {
+      |    "property1": "xxx",
+      |    "property2": 456
+      |  }
+      |}
+      |""".stripMargin
+
+  lazy val genJson = GenObject(
+    GenBoolean("myBoolean"),
+    GenNumber("myInt"),
+    GenString("myString"),
+    GenArray[GenString]("myArray"),
+    GenObject(
+      GenString("property1"),
+      GenNumber("property2")
+    )
+  )
+
+  // TODO allow constants in syntax i.e. Gen.const
+  implicit class ScalaCheckJsonSyntax(key: String) {
+    def gen[T]: Gen[Json] = ???
+  }
+
+  case class MyProperty(property1: String, property2: Int)
+
+  lazy val genJson0 = GenJson0(
+    GenJson0[Boolean]("myBoolean"),
+    GenJson0[Int]("myInt"),
+    GenJson0[String]("myString"),
+    GenJson0[NonEmptyString]("myNonEmptyString"),
+    GenJson0[List[String]]("myArray"),
+    GenJson0[MyProperty]("myObject"),
+    GenJson0[String]("myConstant", "constant")
+  )
+
+  lazy val genJson0Syntax = GenJson0(
+    "myBoolean".gen[Boolean],
+    "myInt".gen[Int],
+    "myString".gen[String],
+    "myNonEmptyString".gen[NonEmptyString],
+    "myArray".gen[List[String]],
+    "myObject".gen[MyProperty]
+  )
+
+}
