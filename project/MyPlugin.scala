11499d9722b499ba0da7d8c0a33f2c3216458e50
niqdev <niqdev@users.noreply.github.com>
2020-04-09 17:06:41 +0100

    add example sbt plugin

---
+++ project/MyPlugin.scala
@@ -0,0 +1,38 @@
+import sbt._
+
+/*
+ * https://www.scala-sbt.org/1.x/docs/Plugins.html
+ * https://www.scala-sbt.org/1.x/docs/Plugins-Best-Practices.html
+ * https://leobenkel.com/2018/11/make-sbt-plugin
+ * https://github.com/freewind/my-sbt-plugin
+ *
+ * sbt 'set logLevel := Level.Debug'
+ */
+object MyPlugin extends AutoPlugin {
+  override def requires = plugins.JvmPlugin
+  override def trigger = allRequirements
+
+  object autoImport {
+    val greeting = settingKey[String]("greeting")
+    val hello = taskKey[Unit]("say hello")
+  }
+
+  import autoImport._
+
+  private[this] lazy val helloTask =
+    Def.task {
+      val log = sbt.Keys.streams.value.log
+      log.info("HELLO_TASK")
+      HelloTask.apply(greeting.value)
+    }
+
+  override lazy val buildSettings = Seq(
+    greeting := "Hi!",
+    hello := helloTask.value
+  )
+}
+
+object HelloTask {
+  def apply(value: String): Unit =
+    println(value)
+}
