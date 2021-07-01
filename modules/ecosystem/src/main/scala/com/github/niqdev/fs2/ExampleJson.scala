f08d42b7c30ecbab1aa8b2b9bf8e3e4571bb3733
niqdev <niqdev@users.noreply.github.com>
2021-07-01 16:06:10 +0100

    update ExampleJson

--- modules/ecosystem/src/main/scala/com/github/niqdev/fs2/ExampleJson.scala
+++ modules/ecosystem/src/main/scala/com/github/niqdev/fs2/ExampleJson.scala
@@ -7,7 +7,7 @@ import _root_.io.circe.fs2.stringArrayParser
 import cats.effect.{ Blocker, ExitCode, IO, IOApp }
 import fs2.data.json.circe._
 import fs2.data.json.selector.root
-import fs2.data.json.{ filter, tokens, values, wrap }
+import fs2.data.json.{ filter, tokens, transform, values, wrap }
 import fs2.{ Stream, io, text }
 
 // http://web.archive.org/web/20201111215332/https://fs2.io
@@ -36,8 +36,8 @@ object ExampleJson extends IOApp {
   }
 
   // https://github.com/satabin/fs2-data/blob/v0.10.0/json/jvm/src/test/scala/fs2/data/json/JsonParsertest.scala
-  private[this] val pathNested = Paths.get(ClassLoader.getSystemResource("fs2-example-nested.json").toURI)
-  private[this] val selector   = root.field("items").iterate.compile
+  private[this] val pathNested    = Paths.get(ClassLoader.getSystemResource("fs2-example-nested.json").toURI)
+  private[this] val itemsSelector = root.field("items").iterate.compile
   // output {}{}{}
   val jsonTokenParser: Stream[IO, Json] = Stream.resource(Blocker[IO]).flatMap { blocker =>
     io.file
@@ -45,7 +45,7 @@ object ExampleJson extends IOApp {
       .through(text.utf8Decode)
       .flatMap(Stream.emits(_))
       .through(tokens)
-      .through(filter(selector))
+      .through(filter(itemsSelector))
       .through(values)
       .evalMap(json => IO.delay { println(s"$json"); json })
   }
@@ -57,7 +57,13 @@ object ExampleJson extends IOApp {
       .through(text.utf8Decode)
       .flatMap(Stream.emits(_))
       .through(tokens)
-      .through(filter(selector))
+      .through(filter(itemsSelector))
+      .through(
+        transform[IO, Json](
+          root.compile, // object selector
+          json => json.deepMerge(Json.obj("newField" -> Json.fromString(s"${java.util.UUID.randomUUID()}")))
+        )
+      )
       .through(wrap.asTopLevelArray)
       .through(values)
       .evalMap(json => IO.delay { println(s"$json"); json })
