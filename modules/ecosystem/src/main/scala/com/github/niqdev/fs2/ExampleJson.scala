111ab3a347309a3031a3e66099c83bcf59a52cc8
niqdev <niqdev@users.noreply.github.com>
2021-07-01 15:10:53 +0100

    update ExampleJson

--- modules/ecosystem/src/main/scala/com/github/niqdev/fs2/ExampleJson.scala
+++ modules/ecosystem/src/main/scala/com/github/niqdev/fs2/ExampleJson.scala
@@ -7,7 +7,7 @@ import _root_.io.circe.fs2.stringArrayParser
 import cats.effect.{ Blocker, ExitCode, IO, IOApp }
 import fs2.data.json.circe._
 import fs2.data.json.selector.root
-import fs2.data.json.{ filter, tokens, values }
+import fs2.data.json.{ filter, tokens, values, wrap }
 import fs2.{ Stream, io, text }
 
 // http://web.archive.org/web/20201111215332/https://fs2.io
@@ -37,18 +37,32 @@ object ExampleJson extends IOApp {
 
   // https://github.com/satabin/fs2-data/blob/v0.10.0/json/jvm/src/test/scala/fs2/data/json/JsonParsertest.scala
   private[this] val pathNested = Paths.get(ClassLoader.getSystemResource("fs2-example-nested.json").toURI)
-  private[this] val sel        = root.field("items").iterate.compile
-  val jsonParser: Stream[IO, Json] = Stream.resource(Blocker[IO]).flatMap { blocker =>
+  private[this] val selector   = root.field("items").iterate.compile
+  // output {}{}{}
+  val jsonTokenParser: Stream[IO, Json] = Stream.resource(Blocker[IO]).flatMap { blocker =>
     io.file
       .readAll[IO](pathNested, blocker, 4096)
       .through(text.utf8Decode)
       .flatMap(Stream.emits(_))
       .through(tokens)
-      .through(filter(sel))
+      .through(filter(selector))
       .through(values)
-      .evalMap(line => IO.delay { println(s"$line"); line })
+      .evalMap(json => IO.delay { println(s"$json"); json })
+  }
+  // output: [{},{},{}]
+  // ".through(wrap.asArrayInObject(at = "events"))" output: {"events":[{},{},{}]}
+  val jsonArrayParser: Stream[IO, Json] = Stream.resource(Blocker[IO]).flatMap { blocker =>
+    io.file
+      .readAll[IO](pathNested, blocker, 4096)
+      .through(text.utf8Decode)
+      .flatMap(Stream.emits(_))
+      .through(tokens)
+      .through(filter(selector))
+      .through(wrap.asTopLevelArray)
+      .through(values)
+      .evalMap(json => IO.delay { println(s"$json"); json })
   }
 
   def run(args: List[String]): IO[ExitCode] =
-    jsonParser.compile.drain.as(ExitCode.Success)
+    jsonArrayParser.compile.drain.as(ExitCode.Success)
 }
